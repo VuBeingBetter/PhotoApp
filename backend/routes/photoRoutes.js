4bdf5adf9a226ccf46b65ea2aba8ffb4deec7324
Nguyễn Quang Vũ <148607209+VuBeingBetter@users.noreply.github.com>
2026-01-30 01:10:40 +0700

    Minor changes using path instead of raw string

--- backend/routes/photoRoutes.js
+++ backend/routes/photoRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const photoController = require('../controllers/photoController');
 const { isAuthenticated } = require('../middleware/auth');
 const multer = require('multer');
 const { ensureDirectoryExists } = require('../utils/fileSystem');
 
 // Run this once at startup to ensure images directory exists
-ensureDirectoryExists(__dirname + '/../images');
+ensureDirectoryExists({ path: path.join(__dirname, '/../images') });
 
 // Configure multer for file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, __dirname + '/../images');
+        cb(null, path.join(__dirname, '/../images'));
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
